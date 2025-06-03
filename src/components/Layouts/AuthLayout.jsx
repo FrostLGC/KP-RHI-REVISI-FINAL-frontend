d64c7375ed7c2ff1c63038873a415c4a6568c287
FrostLGC <alvaherbart.353@gmail.com>
2025-06-03 21:39:11 +0700

    fix logo profile didnt showing

--- src/components/Layouts/AuthLayout.jsx
+++ src/components/Layouts/AuthLayout.jsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import loginImage from '../../assets/images/Logo Profile RHI.jpg';
+import React from "react";
+import loginImage from "../../assets/images/Logo Profile RHI.jpg";
 
 const AuthLayout = ({ children }) => {
   return (
-    <div className='flex'>
-      <div className='w-screen h-screen md:w-[60vw] px-12 pt-8 pb-12'>
-        <h2 className='text-lg font-medium text-black'>
+    <div className="flex">
+      <div className="w-screen h-screen md:w-[60vw] px-12 pt-8 pb-12">
+        <h2 className="text-lg font-medium text-black">
           Project Management RHI Build
         </h2>
         {children}
       </div>
 
-      <div className='hidden md:flex w-[40vw] h-screen items-center justify-center bg-blue-50 overflow-hidden p-8'>
+      <div className="hidden md:flex w-[40vw] h-screen items-center justify-center bg-blue-50 overflow-hidden p-8">
         <img
           src={loginImage}
           alt="Ilustrasi Login"
